0d839db5f4e3cfa71c7844c6cad7d99772a2fd8a
Damian Róg <w57008@student.wsiz.rzeszow.pl>
2018-04-12 18:38:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stud/recycleview/Planeta.java
@@ -0,0 +1,18 @@
+package com.example.stud.recycleview;
+
+/**
+ * Created by W57008 on 2018-04-12.
+ */
+
+public class Planeta {
+    String nazwa;
+    String rodzaj;
+    int ksiezyce;
+
+    public Planeta(String nazwa, String rodzaj, int ksiezyce) {
+        this.nazwa = nazwa;
+        this.rodzaj = rodzaj;
+        this.ksiezyce = ksiezyce;
+    }
+
+}
